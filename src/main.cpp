8f771955c42710ffe0eae108c2828c112160abd0
jeonghopark <jeonghopark@jeonghopark.de>
2015-10-25 19:16:18 +0100

    xcode

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ int main( ){
     settings.width = 1280;  // 1920
     settings.height = 720;  // 1080
     settings.resizable = true;
-    settings.setPosition( ofVec2f(settings.width, 0 ));
+    settings.setPosition( ofVec2f(1440, 0 ));
 //    settings.windowMode = OF_FULLSCREEN;
     shared_ptr<ofAppBaseWindow> mainWindow = ofCreateWindow(settings);
     
